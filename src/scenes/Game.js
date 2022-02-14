866259de9dd9f8e81de3fe5bb2a3cf1a03f53245
SucioLoco <irelandrj@yahoo.com>
2022-02-13 20:30:05 -0500

    added random blocks generation into the grid

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -10,10 +10,13 @@ class Game extends Phaser.Scene
 
     create()
     {
+    this.gridBG;
+
         // Background & Game Board Grid
         this.add.image(400, 200, 'background')
         this.gridBG = this.add.image(675, 300, "blobs", "grid.png")
 
+        // Blobs
         // Orange
         this.blobOrange = this.add.image(325, 100, "blobs", "blob-orange.png")
         this.blobOrange.setAngle(90)
@@ -37,6 +40,33 @@ class Game extends Phaser.Scene
         // Green
         this.blobGreen = this.add.image(1025, 525, "blobs", "blob-green.png")
         this.blobGreen.setDisplaySize(100, 100)
+
+        // Grid
+        this.grid = [];
+        this.frames = ["orange-square.png", "yellow-square.png", "blue-square.png", "pink-square.png", "red-square.png", "green-square.png" ];
+
+        for (let x = 0; x < 14; x++)
+        {
+            this.grid[x] = []
+
+        for (let y = 0; y < 14; y++)
+        {
+            let sx = 444 + (x * 36);
+            let sy = 63 + (y * 36);
+            let color = Phaser.Math.Between(0, 5);
+
+            let block = this.add.image(sx, sy, "blobs", this.frames[color])
+
+            block.setData("oldColor", color);
+            block.setData("color", color);
+            block.setData("x", sx);
+            block.setData("y", sy);
+
+            this.grid[x][y] = block;
+        }
+        }
+        
+
     }
 }
 
